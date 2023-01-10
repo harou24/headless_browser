e29b663f98a8d885d0c55bb43ef5a0287c67c64f
harou24 <haroutioun24@gmail.com>
2023-01-10 22:42:20 +0100

    added take screenshot and take element screenshot on web pages as functionnalities to the headless browser

---
+++ browser/headless.go
@@ -0,0 +1,61 @@
+package browser
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/chromedp/chromedp"
+	"github.com/chromedp/chromedp/device"
+)
+
+type HeadlessBrowser struct {
+	ctx    context.Context
+	cancel context.CancelFunc
+}
+
+func NewHeadlessBrowser() *HeadlessBrowser {
+	ctx, cancel := chromedp.NewContext(context.Background())
+	return &HeadlessBrowser{ctx, cancel}
+}
+
+func (b *HeadlessBrowser) TakeFullScreenshot(url string, pathToSave string) {
+
+	var buf []byte
+	quality := 90
+
+	if err := chromedp.Run(b.ctx, fullScreenshot(url, quality, &buf)); err != nil {
+		log.Fatal(err)
+	}
+	saveInFile(&buf, pathToSave)
+}
+
+func (b *HeadlessBrowser) TakeElementScreenshot(url string, pathToSave string) {
+	var buf []byte
+
+	if err := chromedp.Run(b.ctx, elementScreenshot(url, `img.Homepage-logo`, &buf)); err != nil {
+		log.Fatal(err)
+	}
+	saveInFile(&buf, pathToSave)
+}
+
+func saveInFile(res *[]byte, pathToSave string) {
+	if err := os.WriteFile(pathToSave, *res, 0o644); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func elementScreenshot(url string, selector string, res *[]byte) chromedp.Tasks {
+	return chromedp.Tasks{
+		chromedp.Navigate(url),
+		chromedp.Screenshot(selector, res, chromedp.NodeVisible),
+	}
+}
+
+func fullScreenshot(url string, quality int, res *[]byte) chromedp.Tasks {
+	return chromedp.Tasks{
+		chromedp.Navigate(url),
+		chromedp.FullScreenshot(res, quality),
+		chromedp.Emulate(device.Reset),
+	}
+}
--- go.mod
+++ go.mod
@@ -2,9 +2,20 @@ module github.com/harou24/go_boilerplate
 
 go 1.19
 
-require github.com/spf13/cobra v1.6.1
+require (
+	github.com/chromedp/chromedp v0.8.7
+	github.com/spf13/cobra v1.6.1
+)
 
 require (
+	github.com/chromedp/cdproto v0.0.0-20230109101555-6b041c6303cc // indirect
+	github.com/chromedp/sysutil v1.0.0 // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.1.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.0.0-20220928140112-f11e5e49a4ec // indirect
 )
