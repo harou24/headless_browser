7ab93c106eac291689c09e11b4017fa4388ef5df
harou24 <haroutioun24@gmail.com>
2023-01-11 18:32:43 +0100

    element screenshot test is now passing

--- tests/screenshot_test.go
+++ tests/screenshot_test.go
@@ -17,16 +17,5 @@ func TestTakingFullScreenshot(t *testing.T) {
 func TestTakingElementScreenshot(t *testing.T) {
 	headlessBrowser := browser.NewHeadlessBrowser()
 	defer headlessBrowser.DontForgetToCleanUp()
-
-	// This runs well
-	headlessBrowser.TakeElementScreenshot(`https://pkg.go.dev/`, `img.Homepage-logo`, `./test_element_screenshot.png`)
-
-	// This doesn't run well and produce errors
-	headlessBrowser.TakeElementScreenshot(`https://www.google.com/`, `img.lnXdpd`, `./test_element_screenshot.png`)
-}
-
-func TestTakingElementScreenshot2(t *testing.T) {
-	headlessBrowser := browser.NewHeadlessBrowser()
-	defer headlessBrowser.DontForgetToCleanUp()
 	headlessBrowser.TakeElementScreenshot("https://harou24.github.io/", `div.portfolio-btn`, `./test_element_screenshot.png`)
 }
